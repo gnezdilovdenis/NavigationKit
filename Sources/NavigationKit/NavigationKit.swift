ba0fca292a5df99958a84e25f583a6ebec8267ce
denisgnezdilov <gnezdiloveltech@gmail.com>
2022-05-01 18:25:46 +0300

    Initial Commit

---
+++ Sources/NavigationKit/NavigationKit.swift
@@ -0,0 +1,6 @@
+public struct NavigationKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/NavigationKitTests/NavigationKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NavigationKit
+
+final class NavigationKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NavigationKit().text, "Hello, World!")
+    }
+}
